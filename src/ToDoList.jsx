c1a651d00c1a73be600ea196dd048cff312767e0
rohitsagar203 <155068093+rohitsagar203@users.noreply.github.com>
2024-06-13 17:45:02 +0530

    change4

---
+++ src/ToDoList.jsx
@@ -0,0 +1,35 @@
+// import React,{ useState } from 'react'
+
+// function ToDoList(){
+//     const[task, setTasks] = useState([]);
+//     const[newTask, setNewTask] = useState("");
+
+//     function handleInputChange(event){
+
+//     }
+
+//     function addTask(){
+
+//     }
+
+//     function deleteTask(index){
+
+//     }
+//     function moveTaskUp(index){
+
+//     }
+
+//     function moveTaskUp(index){
+
+//     }
+//     return(
+//     <div className='to-do-list'>
+//         <h1>To-Do-List</h1>
+
+//         <div>
+//             <input
+//              />
+//         </div>
+//     </div>);
+// }
+// export default ToDoList
